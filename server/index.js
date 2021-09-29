d46150fa329ea162f87a25ab63c24e6b86184424
Aryan Biswari <aryanbiswari14@gmail.com>
2021-09-29 12:08:10 +0530

    pern todo list woking

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
 app.listen(4000, () => {
     console.log('listening on 4000');
 });
